a41771fd46a954ea9fa4ffc566b21564c44e8d58
BW-Song <52228016+BW-Song@users.noreply.github.com>
2019-07-22 08:54:20 +0800

    add locateMealPal

--- routes/locateMealPal.py
+++ routes/locateMealPal.py
@@ -3,6 +3,37 @@ Here to implement feature:
 locate a meal pal
 """
 
-def locateMealPal():
-    # to do
+class seat:
+    nameList = []
+    poseList = []
 
+    def __init__(self,pose):
+        seat.poseList = pose
+        seat.nameList = [""] * len(seat.poseList)
+    
+    @classmethod
+    def locateMealPal(self,name):
+        if name in seat.nameList:
+            return self.poseList[self.nameList.index(name)]
+        else:
+            return [0,0]
+        
+
+    def setMyPosition(self,pose,name):
+        seat.nameList[seat.poseList.index(pose)] = name
+
+# usage:
+# if __name__ == '__main__':
+#     a = [0,1]
+#     b = [2,3]
+#     c = [4,5]
+#     pose = [a,b,c]
+#     cat = seat(pose)
+#     cat.setMyPosition(a,"song")
+#     cat.setMyPosition(b,"bo")
+#     cat.setMyPosition(c,"wen")
+#     curPose = cat.locateMealPal("tong")
+#     if curPose == [0,0]:
+#         print("Not in the list")
+#     else:
+#         print(curPose[0])
